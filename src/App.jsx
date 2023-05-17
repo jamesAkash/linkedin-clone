32b12211d760c76ee3100968ab8e201582affaea
jamesAkash <akash.james@outlook.com>
2023-05-17 15:59:20 +0530

    initial commit, clean up

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Lets build</h1>
+
+      {/* Header */}
+      {/* App body */}
+      {/* ------Sidebar */}
+      {/* ------Feed */}
+      {/* ------Widgets */}
+    </div>
+  );
+}
+
+export default App;
